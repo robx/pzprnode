b4fd62cad5223e01371dea41604ca59e73705f43
Robert Vollmert <rob@vllmrt.net>
2019-03-09 17:14:38 +0100

    initial version

---
+++ README.md
@@ -0,0 +1,6 @@
+# PUZ-PRE node render
+
+PUZ-PRE node renderer is a server program to render puzzles in [pzprjs format](http://github.com/sabo2/pzprjs) to PNG.
+
+It relies on [pzprjs](http://github.com/sabo2/pzprjs) and the command line graphics conversion tool [GraphicsMagick](http://graphicsmagick.org).
+
---
+++ app.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const pzpr = require('./dist/pzpr.js');
+const { spawn } = require('child_process');
+const url = require('url');
+
+const hostname = '127.0.0.1';
+const port = 3456;
+
+const server = http.createServer((req, res) => {
+	const canvas = {};
+	const p = new pzpr.Puzzle(canvas);
+	const pzl = url.parse(req.url).query;
+	if (!pzl) {
+		res.statusCode = 400;
+		res.end();
+		return;
+	}
+	p.open(pzl, () => {
+		const svg = p.toBuffer();
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'image/png');
+
+		const gm = spawn('gm', ['convert', 'SVG:-', 'PNG:-']);
+		gm.on('error', (err) => {
+			console.log('error starting gm:', err);
+		});
+		gm.on('close', (code) => {
+			if (code !== 0) {
+				console.log('gm exited with error');
+			}
+			res.end();
+		});
+		gm.stdout.on('data', (data) => {
+			res.write(data);
+		});
+		gm.stdin.end(svg);
+	});
+});
+
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
+
