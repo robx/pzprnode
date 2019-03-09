3af371ad2111d7863f24d993354eab547ab7fcb4
Robert Vollmert <rob@vllmrt.net>
2019-03-09 18:07:50 +0100

    template p.html

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const http = require('http');
 const pzpr = require('./dist/pzpr.js');
 const child_process = require('child_process');
 const url = require('url');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3456;
 
-const server = http.createServer((req, res) => {
+const template = fs.readFileSync('p.html', 'utf8');
+
+function preview(req, res, pzl) {
 	const canvas = {};
 	const p = new pzpr.Puzzle(canvas);
-	const pzl = url.parse(req.url).query;
 	if (!pzl) {
 		res.statusCode = 400;
 		res.end();
@@ -35,6 +37,32 @@ const server = http.createServer((req, res) => {
 		});
 		gm.stdin.end(svg);
 	});
+}
+
+function page(req, res, pzl) {
+	var type_ = pzl.split('/')[0];
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+	res.end(template
+		.replace('%%OG_URL%%', 'http://puzz.link/p?' + pzl)
+		.replace('%%OG_TITLE%%', type_ + ' puzzle')
+		.replace('%%OG_IMAGE%%', 'http://puzz.link/pv?' + pzl));
+}
+
+const server = http.createServer((req, res) => {
+	const u = url.parse(req.url);
+	switch (u.pathname) {
+	case '/pv':
+		preview(req, res, u.query);
+		break;
+	case '/p':
+		page(req, res, u.query);
+		break;
+	default:
+		res.statusCode = 404;
+		res.end();
+		break;
+	}
 });
 
 server.listen(port, hostname, () => {
