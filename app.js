dc112efe019020e53bc7d1099dc86593fe0c030a
Robert Vollmert <rob@vllmrt.net>
2019-03-09 18:22:40 +0100

    heavy-handed parse error handling

--- app.js
+++ app.js
@@ -17,26 +17,32 @@ function preview(req, res, pzl) {
 		res.end();
 		return;
 	}
-	p.open(pzl, () => {
-		const svg = p.toBuffer();
-		res.statusCode = 200;
-		res.setHeader('Content-Type', 'image/png');
+	try {
+		p.open(pzl, () => {
+			const svg = p.toBuffer();
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'image/png');
 
-		const gm = child_process.spawn('gm', ['convert', 'SVG:-', 'PNG:-']);
-		gm.on('error', (err) => {
-			console.log('error starting gm:', err);
+			const gm = child_process.spawn('gm', ['convert', 'SVG:-', 'PNG:-']);
+			gm.on('error', (err) => {
+				console.log('error starting gm:', err);
+			});
+			gm.on('close', (code) => {
+				if (code !== 0) {
+					console.log('gm exited with error');
+				}
+				res.end();
+			});
+			gm.stdout.on('data', (data) => {
+				res.write(data);
+			});
+			gm.stdin.end(svg);
 		});
-		gm.on('close', (code) => {
-			if (code !== 0) {
-				console.log('gm exited with error');
-			}
-			res.end();
-		});
-		gm.stdout.on('data', (data) => {
-			res.write(data);
-		});
-		gm.stdin.end(svg);
-	});
+	} catch (error) {
+		console.log("caught error:", error);
+		res.statusCode = 500;
+		res.end();
+	}
 }
 
 function page(req, res, pzl) {
