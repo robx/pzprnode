5b7cde2d54c8d7f1476e5baf67f1589052f7d2a5
Robert Vollmert <rob@vllmrt.net>
2022-11-16 17:19:41 +0100

    bump to nixpkgs-unstable with fixed librsvg

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   description = "pzprnode";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+  inputs.flake-utils.url = github:numtide/flake-utils;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
   inputs.pzprjs.url = github:robx/pzprjs;
 
   outputs = {
@@ -17,12 +17,6 @@
       in {
         defaultPackage = pkgs.callPackage ./nix/pzprnode.nix {
           pzprjs = pzprjs.defaultPackage.${system};
-          librsvg = pkgs.librsvg.overrideAttrs (old: with pkgs; {
-            version = "patched";
-            patches = [
-              ./nix/patches/librsvg-a1b134bf0a4c6ad884d3ae88a0767563ea68a438.diff
-            ];
-          });
         };
       }
     );
